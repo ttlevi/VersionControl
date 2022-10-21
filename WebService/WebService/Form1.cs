5d67f4f84ae9d1d3974727096bc78f4447237edb
ttlevi <levente.tahi-toth@gmail.com>
2022-10-21 10:48:22 +0200

    3

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebService.MnbServiceReference;
 
 namespace WebService
 {
@@ -15,6 +16,23 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
+
+            CallWebservice();
+        }
+
+        private void CallWebservice()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            
         }
     }
 }
