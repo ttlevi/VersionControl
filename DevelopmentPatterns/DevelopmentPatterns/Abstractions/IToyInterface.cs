f7df37e8f1617a850c7f111d19bd52d39a774698
ttlevi <levente.tahi-toth@gmail.com>
2022-11-04 13:08:43 +0100

    7

---
+++ DevelopmentPatterns/DevelopmentPatterns/Abstractions/IToyInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentPatterns.Abstractions
+{
+    public interface IToyInterface
+    {
+        Toy CreateNew();
+    }
+}
