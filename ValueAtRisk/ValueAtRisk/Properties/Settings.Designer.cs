8975374e71717ef5aadc9566cc364053ef52fa43
ttlevi <levente.tahi-toth@gmail.com>
2022-11-11 12:07:03 +0100

    2-3

--- ValueAtRisk/ValueAtRisk/Properties/Settings.Designer.cs
+++ ValueAtRisk/ValueAtRisk/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ValueAtRisk.Properties
-{
-
-
+namespace ValueAtRisk.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=bit.uni-corvinus.hu;Initial Catalog=Portfolio;Persist Security Info=T" +
+            "rue;User ID=hallgato;Password=Password123")]
+        public string PortfolioConnectionString {
+            get {
+                return ((string)(this["PortfolioConnectionString"]));
             }
         }
     }
