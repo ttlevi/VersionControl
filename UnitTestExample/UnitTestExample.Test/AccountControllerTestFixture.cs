7262e693ba6618c69d8578a0acd6a036654f079d
ttlevi <levente.tahi-toth@gmail.com>
2022-11-25 13:11:14 +0100

    félkész

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("KakaosCsiga", false),
+            TestCase("KAKAOSCS1GA", false),
+            TestCase("kakaoscs1ga", false),
+            TestCase("Cs1ga", false),
+            TestCase("KakaosCs1ga", true)
+
+        ]
+        public void TestValidatePassword()
+        {
+
+        }
+    }
+}
---
+++ UnitTestExample/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b151ddbd-1421-4194-b70d-932d35d76cd3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
