a405b6316a2fb2501a371d8c90165bb7975fd001
ttlevi <levente.tahi-toth@gmail.com>
2022-12-09 12:45:51 +0100

    4

--- EvolutionAlgorythm/EvolutionAlgorythm/Form1.cs
+++ EvolutionAlgorythm/EvolutionAlgorythm/Form1.cs
@@ -16,6 +16,7 @@ namespace EvolutionAlgorythm
     {
         GameController gc = new GameController();
         GameArea ga;
+        Brain winnerBrain = null;
 
         int populationSize = 100;
         int nbrOfSteps = 10;
@@ -38,7 +39,7 @@ namespace EvolutionAlgorythm
             }
 
             gc.GameOver += Gc_GameOver;
-
+            
             gc.Start();
         }
 
@@ -53,6 +54,44 @@ namespace EvolutionAlgorythm
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                button1.Visible = true;
+                return;
+            }
+
+            gc.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
