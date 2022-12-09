7a24eb396c926961a3e9dd8061a2d8f31395d7b9
ttlevi <levente.tahi-toth@gmail.com>
2022-12-09 12:10:01 +0100

    1

--- EvolutionAlgorythm/EvolutionAlgorythm/Form1.cs
+++ EvolutionAlgorythm/EvolutionAlgorythm/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace EvolutionAlgorythm
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            Controls.Add(ga);
+
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+
         }
     }
 }
